37fb43ef4745ca94a1f7224eba78e4b640b2b81e
Vipul Jha <vipuljha08@gmail.com>
2019-09-30 01:12:11 +0530

    new:index: Make title header animated
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- new/js/script.js
+++ new/js/script.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         animation: 'fade',
         play: 5000,
         pagination: false
-        
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Android Developer", "Kernel Developer", "Student"],
+        typeSpeed: 50,
+        loop: true,
+        startDelay: 400,
+        showCursor: false
+
     });
 });
