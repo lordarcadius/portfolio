c1683c7bca0a6b2eece55f7b9319c343c777d01f
Vipul Jha <vipuljha08@gmail.com>
2019-10-03 18:30:20 +0530

    index: Add technical skills section with piecharts
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- new/js/script.js
+++ new/js/script.js
@@ -13,4 +13,47 @@ $(document).ready(function(){
         showCursor: false
 
     });
+
+    $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function () {
+
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+
+
+        }
+
+
+    });
 });
