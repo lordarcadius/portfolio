99df02b323209afef350c6da63728d46652d1a69
Vipul Jha <vipuljha08@gmail.com>
2019-10-06 04:05:59 +0530

    index: Add portfolio section
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- new/js/script.js
+++ new/js/script.js
@@ -69,5 +69,33 @@ $(document).ready(function(){
 
 
     });
+
+    $("[data-fancybox]").fancybox();
+    $(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+    });
+    
+    $("#filters a").click(function(){
+        $("#filters.current").removeClass("current");
+        $(this).addClass("current");
+
+         var selector = $(this).attr("data-filter");
+
+         $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
     
 });
