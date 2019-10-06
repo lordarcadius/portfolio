93676a3e14b046fae3826266a08670d30aa39dc9
Vipul Jha <vipuljha08@gmail.com>
2019-10-06 20:41:30 +0530

    index: Add sticky navbar
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- new/js/script.js
+++ new/js/script.js
@@ -99,5 +99,37 @@ $(document).ready(function(){
 
         return false;
     });
+
+    $("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 80 }, "slow");
+
+	});
+
+    const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+
+
+
+
+	}
     
 });
