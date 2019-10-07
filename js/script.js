dbe9b3ce339552a17285f2b085b4273c1f4c1e49
Vipul Jha <vipuljha08@gmail.com>
2019-10-07 22:42:26 +0530

    Remove easypiechart and owl carousel
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- js/script.js
+++ js/script.js
@@ -33,48 +33,12 @@ $(document).ready(function () {
 
     });
 
-    $('.owl-carousel').owlCarousel({
-        loop: false,
-        nav: false,
-        dots: true,
-        items: 4,
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            938: {
-                items: 4
-            }
-        }
-    });
-
     var skillsTopOffset = $(".skillsSection").offset().top;
     var statsTopOffset = $(".statsSection").offset().top;
     var countUpFinished = false;
 
     $(window).scroll(function () {
-
-        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-
-            $('.chart').easyPieChart({
-                easing: 'easeInOut',
-                barColor: 'rgba(255, 255, 255, 0.8)',
-                trackColor: false,
-                scaleColor: false,
-                lineWidth: 4,
-                size: 152,
-                onStep: function (from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent));
-                }
-            });
-
-        }
+        
         if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
             $(".counter").each(function () {
                 var element = $(this);
