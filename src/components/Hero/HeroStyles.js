a99f753420639079bed6dfc9ea95043502f3002c
Vipul Jha <vipuljha08@gmail.com>
2021-06-18 00:13:22 +0530

    Start migration to React based website The source code is taken from https://github.com/adrianhajdin/portfolio_website I will just modify according to my needs. Full credits goes to Adrian from JavaScript Mastery.
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ src/components/Hero/HeroStyles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const LeftSection = styled.div`
+  width: 100%;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+
+    margin: 0 auto;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    margin: 0 auto;
+  }
+`;
