cf5c11537840c4f467d7ef1005d3ede636754ac1
Vipul Jha <vipuljha08@gmail.com>
2021-06-19 19:38:53 +0530

    ProjectStyles: Left alignment for description instead of justify
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -72,7 +72,7 @@ export const CardInfo = styled.p`
   color: #e4e6e7;
   font-style: 2rem;
   line-height: 24px;
-  text-align: justify;
+  text-align: left;
   @media ${(props) => props.theme.breakpoints.sm} {
     padding:.3rem
   
