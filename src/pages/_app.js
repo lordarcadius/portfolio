07393b35dfe55cc95803d69ba4ddfba46d4e4b79
Vipul Jha <vipuljha08@gmail.com>
2021-06-21 00:18:18 +0530

    Trying to fix Meta image for social media sites
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,20 +8,21 @@ export default function App({ Component, pageProps }) {
     <title>Vipul Jha</title>
           <meta name="title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta name="description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
+          <meta itemprop="image" content="https://www.vipuljha.com/images/banner.jpg"/>
 
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://www.vipuljha.com/" />
           <meta property="twitter:title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta property="twitter:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="twitter:image" content="/images/banner.jpg" />
+          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.jpg" />
 
           {/* Open Graph */}
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.vipuljha.com" />
           <meta property="og:title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta property="og:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="og:image" content="/images/banner.jpg" />
+          <meta property="og:image" content="https://www.vipuljha.com/images/banner.jpg" />
     </Head>
       <Theme>
         <Component {...pageProps} />
