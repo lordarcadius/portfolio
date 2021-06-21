3d02f835f7036b57f8c3da9cf1691dca838253fe
Vipul Jha <vipuljha08@gmail.com>
2021-06-21 22:30:03 +0530

    _app.js: Update banner
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,21 +8,21 @@ export default function App({ Component, pageProps }) {
     <title>Vipul Jha</title>
           <meta name="title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta name="description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta itemprop="image" content="https://www.vipuljha.com/images/banner.jpg"/>
+          <meta itemprop="image" content="https://www.vipuljha.com/images/banner.png"/>
 
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://www.vipuljha.com/" />
           <meta property="twitter:title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta property="twitter:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.jpg" />
+          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" />
 
           {/* Open Graph */}
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.vipuljha.com" />
           <meta property="og:title" content="Vipul Jha - Co-founder, Hexoncode" />
           <meta property="og:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="og:image" content="https://www.vipuljha.com/images/banner.jpg" />
+          <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
     </Head>
