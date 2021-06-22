689fb32d4072951cf5b6699289b62734228fc245
Vipul Jha <vipuljha08@gmail.com>
2021-06-22 14:57:18 +0530

    FFS ;___; Why the fcuk meta preview ain't working
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,16 +6,6 @@ export default function App({ Component, pageProps }) {
     <>
     <Head>
     <title>Vipul Jha</title>
-          <meta name="title" content="Vipul Jha - Co-founder, Hexoncode" />
-          <meta name="description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Vipul Jha - Co-founder, Hexoncode" />
-          <meta property="og:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content="Vipul Jha - Co-founder, Hexoncode" />
-          <meta property="twitter:description" content="Hey there, This is Vipul Jha. I am the co-founder of Hexoncode. I develop Apps, Kernels, ROMs, and Scripts." />
-          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" />
     </Head>
       <Theme>
         <Component {...pageProps} />
