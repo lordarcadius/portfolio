6a994ba60a55a171fd0e7b395289c61e1bdd1d01
Vipul Jha <vipuljha08@gmail.com>
2021-08-24 00:10:20 +0530

    TimeLineStyles: Fix CarouselItem background on mobile devices
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/components/TimeLine/TimeLineStyles.js
+++ src/components/TimeLine/TimeLineStyles.js
@@ -51,7 +51,7 @@ export const CarouselItem = styled.div`
   @media ${props => props.theme.breakpoints.sm} {
     margin-left: 32px;
     min-width: 120px;
-    background: #0E131F;
+    background: #0F1624;
     padding: 4px;
     align-content: start;
     scroll-snap-align: start;
