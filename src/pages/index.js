ba5e2a540180bd05eb27a2c3e5a71082b1963c35
Vipul Jha <vipuljha08@gmail.com>
2024-04-20 22:17:58 +0530

    Remove timeline
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation'
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
@@ -16,7 +15,6 @@ const Home = () => {
       </Section>
       <Projects />
       <Technologies />
-      <Timeline />
       <Acomplishments />
     </Layout>
   );
